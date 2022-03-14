85aeac9b161fa7503f08a55e1db8fc69d045194b
warm113 <1135375289@qq.com>
2022-03-14 14:29:23 +0800

    1 file modified

--- src/views/rule/columns.ts
+++ src/views/rule/columns.ts
@@ -12,5 +12,25 @@ const columns = [
     key: 'groupName',
     align: 'center',
   },
+  {
+    title: '考勤人数',
+    dataIndex: 'num',
+    key: 'num',
+    align: 'center',
+    scopedSlots: { customRender: 'num' },
+  },
+  {
+    title: '当前状态',
+    dataIndex: 'state',
+    key: 'state',
+    align: 'center',
+    scopedSlots: { customRender: 'state' },
+  },
+  {
+    title: '操作',
+    key: 'action',
+    scopedSlots: { customRender: 'action' },
+    align: 'center',
+  },
 ];
 export default columns;
